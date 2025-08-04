1c965b804e23a1e7ec71b189f7d43762f7a8fc6b
Alberto Garcia <berto@igalia.com>
2025-08-04 15:04:39 +0200

    Check all FIDO2 credentials, not just the first one
    Closes #2

--- keyscript.sh
+++ keyscript.sh
@@ -1,17 +1,24 @@
 #!/bin/sh
 
 cleanup () {
-    rm -f "$ASSERT_PARAMS" "$LUKS_TOKEN"
+    rm -f "$ASSERT_PARAMS" "$LUKS_TOKEN" "$LUKS_TOKEN_LIST"
 }
 
 ASSERT_PARAMS=$(mktemp -t params.XXXXXX)
+LUKS_TOKEN_LIST=$(mktemp -t tokenlist.XXXXXX)
 LUKS_TOKEN=$(mktemp -t token.XXXXXX)
 trap cleanup INT EXIT
 
 cryptsetup luksDump --dump-json-metadata "$CRYPTTAB_SOURCE" | \
-    jq -e '[.tokens[] | select(."fido2-credential" != null)][0]' > "$LUKS_TOKEN"
+    jq -e '[.tokens[] | select(."fido2-credential" != null)]' > "$LUKS_TOKEN_LIST"
 
-if [ $? -ne 0 ]; then
+if [ $? -eq 0 ]; then
+    NTOKENS=$(jq length "$LUKS_TOKEN_LIST")
+else
+    NTOKENS=0
+fi
+
+if [ "$NTOKENS" = "0" ]; then
     echo "*** No FIDO2 credentials found in $CRYPTTAB_SOURCE" >&2
 else
     echo "*** Waiting for a FIDO2 authenticator..." >&2
@@ -25,33 +32,51 @@ else
         echo "*** Found FIDO2 authenticator $FIDO2_AUTHENTICATOR" >&2
         FIDO2_DEV=${FIDO2_AUTHENTICATOR%%:*}
 
-        REQ_PIN=$(jq -r '."fido2-clientPin-required"' "$LUKS_TOKEN")
-        REQ_UP=$(jq -r '."fido2-up-required"' "$LUKS_TOKEN")
+        VALID_CREDENTIAL="no"
+        for i in $(seq "$NTOKENS"); do
+            jq ".[$i-1]" "$LUKS_TOKEN_LIST" > "$LUKS_TOKEN"
+            jq -r '"AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=",
+                   ."fido2-rp",
+                   ."fido2-credential",
+                   ."fido2-salt"' "$LUKS_TOKEN" > "$ASSERT_PARAMS"
+            if fido2-assert -G -t up=false -t pin=false -i "$ASSERT_PARAMS" \
+                            -o /dev/null "$FIDO2_DEV" 2> /dev/null; then
+                VALID_CREDENTIAL="yes"
+                break
+            fi
+        done
 
-        jq -r '"AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=",
+        if [ "$VALID_CREDENTIAL" = "yes" ]; then
+            REQ_PIN=$(jq -r '."fido2-clientPin-required"' "$LUKS_TOKEN")
+            REQ_UP=$(jq -r '."fido2-up-required"' "$LUKS_TOKEN")
+
+            jq -r '"AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=",
                ."fido2-rp",
                ."fido2-credential",
                ."fido2-salt"' "$LUKS_TOKEN" > "$ASSERT_PARAMS"
 
-        if [ "$REQ_PIN" = "true" ]; then
-            stty -echo
-        fi
+            if [ "$REQ_PIN" = "true" ]; then
+                stty -echo
+            fi
 
-        sleep 2
+            sleep 2
 
-        SECRET=$(fido2-assert -G -h -t up="$REQ_UP" -t pin="$REQ_PIN" \
-                              -i "$ASSERT_PARAMS" "$FIDO2_DEV" | tail -n 1)
+            SECRET=$(fido2-assert -G -h -t up="$REQ_UP" -t pin="$REQ_PIN" \
+                                  -i "$ASSERT_PARAMS" "$FIDO2_DEV" | tail -n 1)
 
-        if [ "$REQ_PIN" = "true" ]; then
-            stty echo
-        fi
+            if [ "$REQ_PIN" = "true" ]; then
+                stty echo
+            fi
 
-        if [ -n "$SECRET" ]; then
-            echo >&2
-            printf "%s" "$SECRET"
-            exit 0
+            if [ -n "$SECRET" ]; then
+                echo >&2
+                printf "%s" "$SECRET"
+                exit 0
+            else
+                echo "*** Error obtaining secret from $FIDO2_DEV" >&2
+            fi
         else
-            echo "*** Error obtaining secret from $FIDO2_DEV" >&2
+            echo "*** No valid credential found for this FIDO2 authenticator" >&2
         fi
     else
         echo "*** No FIDO2 authenticator found" >&2
